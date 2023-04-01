757ca6dca2cbc8a3f5e00df3fd8c2009073e7f52
Javaiary <narrchoii@gmail.com>
2023-04-01 16:39:20 +0900

    가장 큰 수 찾기

---
+++ CodingTests/src/programmers/FindingMax.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class FindingMax {
+	public int[] solution(int[] array) {
+		int[] answer = new int[2];
+		int max = 0;
+		for (int i = 0; i < array.length; i++) {
+			max = Math.max(max, array[i]);
+			if (max == array[i]) {
+				answer[1] = i;
+			}
+		}
+		answer[0] = max;
+
+		return answer;
+	}
+}
